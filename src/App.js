65bf47670cbb6d680b1a6b9ffaccf8163b5ca14c
ted <ted@warpedpuppy.com>
2023-04-26 12:17:07 -0400

    add code

--- src/App.js
+++ src/App.js
@@ -1,98 +1,106 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 class App extends React.Component {
-  state = {code: '', events: [], error: '', testing: true}
+  state = { code: "", events: [], error: "", testing: true };
   componentDidMount = () => {
+    if (!this.state.testing) {
+      this.testEndPoints();
+    } else {
+      const queryString = window.location.search;
+      console.log(queryString);
+      const urlParams = new URLSearchParams(queryString);
+	  const code = urlParams.get('code')
+      console.log('code', code);
+	  this.setState({code})
+    }
+  };
 
-    if (!this.state.testing) this.testEndPoints();
-  }
-
-  async testEndPoints () {
-
+  async testEndPoints() {
     const queryString = window.location.search;
-    
+
     const urlParams = new URLSearchParams(queryString);
 
     let code = urlParams.get("code");
 
     if (!code) {
-
-      let result = await fetch('https://m200p3c8ne.execute-api.us-east-1.amazonaws.com/dev/api/get-auth-url');
+      let result = await fetch(
+        "https://m200p3c8ne.execute-api.us-east-1.amazonaws.com/dev/api/get-auth-url"
+      );
       let resultJson = await result.json();
       let { authUrl } = resultJson;
-	  console.log(authUrl)
+      console.log(authUrl);
       window.location.href = authUrl;
-
     } else {
-		
       if (decodeURIComponent(code) === code) {
-          code = encodeURIComponent(code);
+        code = encodeURIComponent(code);
       }
-	  console.log('get code ', code);
-      let result = await fetch(`https://m200p3c8ne.execute-api.us-east-1.amazonaws.com/dev/api/token/${code}`)
+      console.log("get code ", code);
+      let result = await fetch(
+        `https://m200p3c8ne.execute-api.us-east-1.amazonaws.com/dev/api/token/${code}`
+      );
       let resultJson = await result.json();
 
-	  console.log('get token call result', resultJson);
-
+      console.log("get token call result", resultJson);
 
       const { access_token } = resultJson;
-      let calendarCall = await fetch(`https://m200p3c8ne.execute-api.us-east-1.amazonaws.com/dev/api/get-events/${access_token}`);
+      let calendarCall = await fetch(
+        `https://m200p3c8ne.execute-api.us-east-1.amazonaws.com/dev/api/get-events/${access_token}`
+      );
       let calendarCallJSON = await calendarCall.json();
 
-	  console.log("calendar", calendarCall)
-	  console.log("errors", calendarCallJSON.errors)
-
+      console.log("calendar", calendarCall);
+      console.log("errors", calendarCallJSON.errors);
 
       if (calendarCallJSON.errors) {
-		this.setState({error: Array.isArray(calendarCallJSON.errors) ? calendarCallJSON.errors[0].message : calendarCallJSON.errors.message })
+        this.setState({
+          error: Array.isArray(calendarCallJSON.errors)
+            ? calendarCallJSON.errors[0].message
+            : calendarCallJSON.errors.message,
+        });
         // this.goToRoot();
       } else {
-        this.setState({events: calendarCallJSON.events})
+        this.setState({ events: calendarCallJSON.events });
       }
     }
   }
 
   goToRoot = () => {
     window.location.href = `${window.location.origin}/meet/`;
-  }
+  };
 
-  
+  render() {
+    const { events, error, testing, code } = this.state;
 
-  render () {
+    if (testing) return (
+	<>
+	<h1>hello.</h1>
+	<h2>here is the code from the url string: {code} </h2>
+	</>
+	);
 
-	
-    const { events, error, testing } = this.state;
+    if (error) {
+      return (
+        <div className="App">
+          <h1>error: {error}</h1>
+        </div>
+      );
+    }
 
-	if (testing) return <h1>hello.</h1>
-
-
-	if (error) {
-		return (
-			<div className="App">
-				<h1>error: { error }</h1>
-			</div>
-		)
-	}
-
-     return (
+    return (
       <div className="App">
-		<h1>event list:</h1>
+        <h1>event list:</h1>
         <button onClick={this.goToRoot}>refresh</button>
         <hr />
         <ul>
-          {
-            events.map( (item, i) => {
-              return <li key={i}>{item.summary}</li>
-            })
-          }
+          {events.map((item, i) => {
+            return <li key={i}>{item.summary}</li>;
+          })}
         </ul>
         <hr />
       </div>
     );
   }
-  
- 
 }
 
 export default App;
